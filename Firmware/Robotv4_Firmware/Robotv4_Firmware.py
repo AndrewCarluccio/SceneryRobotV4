29db4e29f58789d366f99dcc72c57cdc0b03f2f1
AndrewCarluccio <akclucc@gmail.com>
2020-01-27 17:07:27 -0500

    Talkback from robot

--- Firmware/Robotv4_Firmware/Robotv4_Firmware.py
+++ Firmware/Robotv4_Firmware/Robotv4_Firmware.py
@@ -1,15 +1,15 @@
 # Complete example of using the RoboClaw library
 #import the relevant code from the RoboClaw library
 
-from roboclaw import RoboClaw
+from roboclaw import Roboclaw
 
 # address of the RoboClaw as set in Motion Studio
 
-address = 0x128
+address = 128
 
 # Creating the RoboClaw object, serial port and baudrate passed
 
-roboclaw = RoboClaw(“/dev/ttyS0”, 115200)
+roboclaw = Roboclaw("/dev/ttyS0", 115200)
 
 # Starting communication with the RoboClaw hardware
 
@@ -18,3 +18,6 @@ roboclaw.Open()
 # Start motor 1 in the forward direction at half speed
 
 roboclaw.ForwardM1(address, 63)
+
+while(True):
+    a=1
