0095e0b15fbff85e50be9a7408f63785e140e083
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2020-01-28 14:52:34 -0500

    Update Robotv4_Firmware.py

--- Firmware/Robotv4_Firmware/Robotv4_Firmware.py
+++ Firmware/Robotv4_Firmware/Robotv4_Firmware.py
@@ -83,7 +83,7 @@ while(True):
 
     elif(words[0] == "cue"):
         #SpeedAccelDeccelPositionM1(self,address,accel,speed,deccel,position,buffer)
-        SpeedAccelDeccelPositionM1(self,address,words[1],words[2],words[3],words[4],words[5])
+        roboclaw.SpeedAccelDeccelPositionM1(address,words[1],words[2],words[3],words[4],words[5])
     #else:
         #print ("No data")
 
