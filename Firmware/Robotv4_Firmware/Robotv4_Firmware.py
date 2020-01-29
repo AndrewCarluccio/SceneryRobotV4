ec342da77b6e8597e12e703b6da2568f25f58a81
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2020-01-29 13:37:00 -0500

    Update Robotv4_Firmware.py

--- Firmware/Robotv4_Firmware/Robotv4_Firmware.py
+++ Firmware/Robotv4_Firmware/Robotv4_Firmware.py
@@ -66,7 +66,7 @@ def pin_mode(pin, val):
 roboclaw.SetEncM1(address, 0)
 roboclaw.SetEncM2(address, 0)
 
-full_rotation = 4218.048
+full_rotation = 4218.048*2.0
 
 #Execution loop
 while(True):
@@ -108,10 +108,10 @@ while(True):
         time.sleep(0.1)
         roboclaw.ForwardM1(address, 0) 
     elif(words[0] == "spin1"):
-        roboclaw.SpeedAccelDeccelPositionM1(address,30,50,20,int(full_rotation),1)
+        roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation),1)
     elif(words[0] == "spin2"):
-        roboclaw.SpeedAccelDeccelPositionM1(address,30,50,20,int(full_rotation / 2.0),1)
-        roboclaw.SpeedAccelDeccelPositionM1(address,30,50,20,int(full_rotation / 2.0),1)
+        roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation / 2.0),1)
+        roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation / 2.0),1)
         
 
     #else:
