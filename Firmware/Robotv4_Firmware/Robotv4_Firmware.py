0d79d917dd82c48f7a33ead271a369b27a531746
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2020-01-30 14:03:56 -0500

    360

--- Firmware/Robotv4_Firmware/Robotv4_Firmware.py
+++ Firmware/Robotv4_Firmware/Robotv4_Firmware.py
@@ -110,8 +110,8 @@ while(True):
     elif(words[0] == "spin1"):
         roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation),1)
     elif(words[0] == "spin2"):
-        roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation / 4.0),1)
-        roboclaw.SpeedAccelDeccelPositionM2(address,300,500,200,int(full_rotation / -4.0),1)
+        roboclaw.SpeedAccelDeccelPositionM1(address,300,500,200,int(full_rotation / 2.0),1)
+        roboclaw.SpeedAccelDeccelPositionM2(address,300,500,200,int(full_rotation / -2.0),1)
         
 
     #else:
