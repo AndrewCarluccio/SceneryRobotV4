4784bb542ddd3e130e73a5fb7b47697ff18138b0
Andy Carluccio <45571628+AndrewCarluccio@users.noreply.github.com>
2020-02-17 14:26:13 -0500

    Update Robotv4_Firmware.py

--- Firmware/Robotv4_Firmware/Robotv4_Firmware.py
+++ Firmware/Robotv4_Firmware/Robotv4_Firmware.py
@@ -72,7 +72,7 @@ def curve_test():
     # Set control points
     crv.ctrlpts = [[1, 0], [1, 1]]
     # Set knot vector
-    crv.knotvector = [5, 0, 3, 1]
+    crv.knotvector = [1, 0, 1, 1]
     
     # Get curve points
     points = crv.evalpts
